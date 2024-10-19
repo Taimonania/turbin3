06d1b186c28bcb2f49f8ecf423b8c95834f8e824
Timo Angerer <taimonania+github@gmail.com>
2024-10-19 11:33:53 +0200

    Add README for scripts

---
+++ .scripts/README.md
@@ -0,0 +1,24 @@
+# Script Overview
+
+## sync-turbin3-fork.sh
+
+This script syncs the personal repo with the turbin3 fork.
+
+In essence it runs these commands:
+
+```bash
+git remote add $FORK_REMOTE_NAME $FORK_REPO_URL
+git push $FORK_REMOTE_NAME main
+```
+
+### Options
+
+Run it with `--force` to run the git push command forcefully.
+
+### Setup
+
+`FORK_REPO_URL`: Put the SSH url of the fork here
+
+`SOURCE_BRANCH`: Name of the branch you want to push to the fork (usually `main`)
+
+`FORK_REMOTE_NAME`: Put the name for the git remote that the fork will be added (this can be any arbitrary name, e.g. `turbin3-fork`)
--- .scripts/sync-turbin3-fork.sh
+++ .scripts/sync-turbin3-fork.sh
@@ -3,12 +3,19 @@
 # Fail the script if any command fails
 set -e
 
+### Configuration START ###
+
 # URL of the fork you want to sync
 FORK_REPO_URL="git@github.com-personal:solana-turbin3/Q4_Sol_Taimonania.git" 
+# Name of the source branch to push from
+SOURCE_BRANCH="main"
+# Name for the git remote fork (can be anything)
+FORK_REMOTE_NAME="turbin3-fork" 
+
+### Configuration END ###
 
 # Navigate to the current directory where the script is run
 REPO_PATH="$(pwd)"
-FORK_REMOTE_NAME="turbin3-fork" # Name for the git remote fork (can be anything)
 
 # Ensure we're in a git repository
 if [ ! -d "$REPO_PATH/.git" ]; then
@@ -16,16 +23,26 @@ if [ ! -d "$REPO_PATH/.git" ]; then
     exit 1
 fi
 
+# Check for the '--force' argument
+FORCE_PUSH="false"
+if [[ "$1" == "--force" ]]; then
+    FORCE_PUSH="true"
+fi
+
 # Check that we're on the main branch
-git checkout main
+git checkout $SOURCE_BRANCH
 
 # Fetch latest changes from the original repository
-git pull origin main
+git pull origin $SOURCE_BRANCH
 
 # Add the fork as a remote if not already added
 git remote get-url "$FORK_REMOTE_NAME" &>/dev/null || git remote add "$FORK_REMOTE_NAME" "$FORK_REPO_URL"
 
-# Push the changes to the fork's main branch
-git push "$FORK_REMOTE_NAME" main
+# Push the changes to the fork's main branch, optionally with --force
+if [[ "$FORCE_PUSH" == "true" ]]; then
+    git push "$FORK_REMOTE_NAME" $SOURCE_BRANCH --force
+else
+    git push "$FORK_REMOTE_NAME" $SOURCE_BRANCH
+fi
 
 echo "Fork has been successfully synced with the original repository."
